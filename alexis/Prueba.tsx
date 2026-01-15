63d8833c54808ac6eacba4c7cee99d31c506c575
Ajpanchi <alexisj131294@hotmail.com>
2026-01-14 19:15:11 -0500

    cambio

---
+++ alexis/Prueba.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Prueba() {
+  return (
+    <View>
+      <Text>Prueba</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
