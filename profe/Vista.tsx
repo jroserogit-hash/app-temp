b9e9af8d1238ada0a96699f72d58922dd4bc0284
jroserogit-hash <jroserogit@gmail.com>
2026-01-14 19:24:47 -0500

    cambios

--- profe/Vista.tsx
+++ profe/Vista.tsx
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Vista() {
   return (
     <View>
-      <Text>Vista</Text>
+      <Text style={ styles. texto}>Vista</Text>
+      <Button title='boton'/>
+      <Text>HOLA</Text>
+      <ActivityIndicator/>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    texto:{
+        fontSize:50
+    }
+})
