5e8b776b7c25d9b985b958da8307c4003be1cdc6
Ajpanchi <alexisj131294@hotmail.com>
2026-01-14 19:28:35 -0500

    cambio2

--- profe/Vista.tsx
+++ profe/Vista.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Vista() {
   return (
     <View>
-      <Text>Vista</Text>
+      <Text style={styles.texto}>Vista</Text>
+      <Button title='Guardar'/>
+      <Text style={styles.texto}>Vista</Text>
+      <Text style={styles.texto}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi qui debitis asperiores praesentium. Adipisci animi, magni illo, alias incidunt quae tenetur, reiciendis voluptatem est omnis harum pariatur unde. Numquam, impedit!</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  texto:{
+    fontSize:20,
+
+
+  }
+
+
+})
